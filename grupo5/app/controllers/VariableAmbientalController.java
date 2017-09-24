059a7ac0d2b5d46a7ff86dba901be8ca239cd661
a.echeverrir <a.echeverrir@uniandes.edu.co>
2017-09-24 13:50:39 -0500

    variable ambiental model y controller

---
+++ grupo5/app/controllers/VariableAmbientalController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
+import models.VariableAmbiental;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+import repositories.RegistroMedicionRepository;
+
+/**
+ * Created by a.echeverrir on 24/09/2017.
+ */
+public class VariableAmbientalController extends Controller
+{
+
+
+
+}
