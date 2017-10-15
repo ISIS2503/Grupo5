a4f68226f302b38b4d8cfd429049ae44f2774fc0
William Ravelo <wr.ravelo@uniandes.edu.co>
2017-10-14 19:36:30 -0500

    se hicieron los test de integracion

--- grupo5/app/models/api/registro_medicion.rb
+++ grupo5/app/models/api/registro_medicion.rb
@@ -5,4 +5,8 @@ class Api::RegistroMedicion
   field :valor, type: Float
   field :tipo, type: String
   field :unidad, type: String
+
+  validates :valor, presence: true
+  validates :tipo, presence: true
+  validates :unidad, presence: true
 end
