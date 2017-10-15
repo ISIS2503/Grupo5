b7cb6eb4d4094fa22c560314488aa4dc02066984
William Ravelo <wr.ravelo@uniandes.edu.co>
2017-10-14 23:57:39 -0500

    se empezo a agregar conexion con servicio al script

--- scripts/consumidorMosquitto.rb
+++ scripts/consumidorMosquitto.rb
@@ -1,6 +1,27 @@
 require 'rubygems'
 require 'mqtt'
 require 'json'
+require 'net/http'
+require 'uri'
+
+def create_register(tipo, unidad, valor)
+  uri = URI.parse('http://localhost:3000/api/registro_mediciones')
+  header = {'Content-Type': 'text/json'}
+
+  payload = {
+    "registro_medicion": {
+      "tipo": tipo,
+      "unidad": unidad,
+      "valor": valor
+    }
+  }
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Post.new(uri.request_uri, header)
+  request.body = payload.to_json
+
+  response = http.request(request)
+end
 
 client = MQTT::Client.connect(:host => '172.24.42.32', :port => 8083 )
 client.subscribe( 'alta.piso1.local1' )
@@ -11,6 +32,8 @@ client.get do |topic,message|
   puts "-----------------------------------------"
   json = JSON.parse(message)
   puts json
+
+  create_register("","","")
 end
 
 client.disconnect()
