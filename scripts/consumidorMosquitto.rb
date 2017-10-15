2092b80024f8bbfdc6a3b9896ccb72343f6d93cc
William Ravelo <wr.ravelo@uniandes.edu.co>
2017-10-15 00:16:14 -0500

    se acabo script consumidor de mosquitto

--- scripts/consumidorMosquitto.rb
+++ scripts/consumidorMosquitto.rb
@@ -6,13 +6,13 @@ require 'uri'
 
 def create_register(tipo, unidad, valor)
   uri = URI.parse('http://localhost:3000/api/registro_mediciones')
-  header = {'Content-Type': 'text/json'}
+  header = {'Content-Type': 'application/json'}
 
   payload = {
     "registro_medicion": {
+      "valor": valor,
       "tipo": tipo,
-      "unidad": unidad,
-      "valor": valor
+      "unidad": unidad
     }
   }
 
@@ -21,6 +21,7 @@ def create_register(tipo, unidad, valor)
   request.body = payload.to_json
 
   response = http.request(request)
+  puts "#{response.code} #{tipo}"
 end
 
 client = MQTT::Client.connect(:host => '172.24.42.32', :port => 8083 )
@@ -33,7 +34,11 @@ client.get do |topic,message|
   json = JSON.parse(message)
   puts json
 
-  create_register("","","")
+  create_register("Temperature",json['Temperature']['Unit'], json['Temperature']['Value'])
+  create_register("Sound",json['Sound']['Unit'], json['Sound']['Value'])
+  create_register("Monoxide",json['Monoxide']['Unit'], json['Monoxide']['Value'])
+  create_register("Lux",json['Lux']['Unit'], json['Lux']['Value'])
+  # TODO Mandar el place
 end
 
 client.disconnect()
