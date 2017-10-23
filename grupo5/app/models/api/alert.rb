a22432466fc10ce23966a8af7f58b6cb346e620c
William Ravelo <wr.ravelo@uniandes.edu.co>
2017-10-23 13:57:59 -0500

    se comenzaron las relaciones

--- grupo5/app/models/api/alert.rb
+++ grupo5/app/models/api/alert.rb
@@ -1,6 +1,8 @@
 class Api::Alert
   include Mongoid::Document
-  field :fecha, type: Date
+  #field :fecha, type: Date
   field :tipo, type: Integer
   field :mensaje, type: String
+
+  belongs_to :microcontrolador, :class_name => 'Api::Microcontrolador'
 end
--- grupo5/app/models/api/microcontrolador.rb
+++ grupo5/app/models/api/microcontrolador.rb
@@ -2,4 +2,6 @@ class Api::Microcontrolador
   include Mongoid::Document
   field :nivel, type: Integer
   field :area, type: Integer
+
+  has_many :alerts, :class_name => 'Api::Alert'
 end
