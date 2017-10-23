1a60729b7a9d0285f8cdecda9b5c21051de8268f
a.echeverrir <a.echeverrir@uniandes.edu.co>
2017-10-23 11:40:07 -0500

    variable ambiental get

--- grupo5/config/routes.rb
+++ grupo5/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     resources :registro_mediciones, only: [:create, :index, :show, :destroy]
-     resources :microcontrolador, only: [:index]
+    resources :microcontrolador, only: [:index]
+    resources :variable_ambiental, only: [:index]
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
