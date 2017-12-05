e89fcd8e463141dd193aa457c8936da3f3bc954a
William Ravelo <drummerwilliam@gmail.com>
2017-12-05 16:45:50 -0500

    se acabo de mejorar el seeds

--- grupo5/db/seeds.rb
+++ grupo5/db/seeds.rb
@@ -6,14 +6,6 @@ Api::RegistroMedicion.delete_all
 Api::VariableAmbiental.delete_all
 Delayed::Job.delete_all
 
-varAlerta = Api::Alert.new(tipo:1, mensaje:"alerta fuera de rango: valor recibido=1 valor minimo=2.0")
-
-varActuador = Api::Actuador.new(estado:false)
-
-varMicrocontrolador = Api::Microcontrolador.new(nivel:1,area:1)
-
-varRegistroMedicion = Api::RegistroMedicion.new(valor:1)
-
 varVariableAmbiental1 = Api::VariableAmbiental.new(tipo:"Temperature", valorMaximo:27.0, valorMinimo:21.5 , unidad:"∞C", variacionDiaria:5.4 , precision:2.0 , frecuencia:60)
 
 varVariableAmbiental2 = Api::VariableAmbiental.new(tipo:"Sound", valorMaximo:85.0, valorMinimo:0.0 , unidad:"dB", variacionDiaria:0.0 , precision:2.0 , frecuencia:120)
@@ -24,23 +16,34 @@ varVariableAmbiental4 = Api::VariableAmbiental.new(tipo:"Lux", valorMaximo:2000.
 
 varUsuario = Api::Usuario.new(nombre:"Alejandro", correo:"a.echeverrir@uniandes.edu.co", contrasena:"password1234" )
 
-varActuador.microcontrolador = varMicrocontrolador
-varActuador.variable_ambiental = varVariableAmbiental1
-
-varAlerta.microcontrolador = varMicrocontrolador
-
-varRegistroMedicion.variable_ambiental = varVariableAmbiental1
-varRegistroMedicion.microcontrolador =  varMicrocontrolador
-
 varVariableAmbiental1.save
 varVariableAmbiental2.save
 varVariableAmbiental3.save
 varVariableAmbiental4.save
 
-varMicrocontrolador.save
-varRegistroMedicion.save
-varAlerta.save
-varActuador.save
+i = 0
+while i < 10 do
+  j = 0
+  currentActuador = Api::Actuador.new(estado:false)
+  while(j < 10) do
+    currentMicrocontrolador = Api::Microcontrolador.new(nivel: i ,area: j)
+    currentMicrocontrolador.save
+    j+= 1
+  end
+  currentActuador.microcontrolador = currentMicrocontrolador
+  currentActuador.variable_ambiental = Api::VariableAmbiental.all.sample
+  currentActuador.save
+  i+= 1
+end
+
+i = 0
+while i < 10 do
+  currentAlerta = Api::Alert.new(tipo: rand(0...2), mensaje:"alerta: valor recibido=1 valor minimo=2.0")
+  currentAlerta.microcontrolador = Api::Microcontrolador.all.sample
+  currentAlerta.save
+  i += 1
+end
+
 varUsuario.save
 
 i = 0
@@ -49,7 +52,7 @@ while i < 50 do
   while(j < 50) do
     currentRegistro = Api::RegistroMedicion.new(valor: rand(1...30))
     currentRegistro.variable_ambiental = Api::VariableAmbiental.all.sample
-    currentRegistro.microcontrolador =  varMicrocontrolador
+    currentRegistro.microcontrolador =  Api::Microcontrolador.all.sample
     currentRegistro.save
     j+= 1
   end
